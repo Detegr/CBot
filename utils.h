6f1e4aaa4f0e25068ebb5c4643d5cbcebf21370d
Dtgr <detegr@gmail.com>
2011-07-18 22:53:50 +0300

    Removed the old-ugly-config-system

--- utils.h
+++ utils.h
@@ -1,18 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-struct config
-{
-   char** authorized_users;
-   char* bot_name;
-   char* bot_realname;
-};
-
-void config_create();
-void config_get(struct config* config);
-void config_adduser(struct config* config, const char* user);
-int config_writeuser(const char* user);
-void config_destroy(struct config* config);
 void concat(char* to, int n, ...);
 
 #endif
