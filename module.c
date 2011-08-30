6d85d1d2d22bcb845a47d2fa533d0e4b19022ac3
Detegr <@>
2011-08-30 21:27:17 +0300

    Hotfix to crash.

--- module.c
+++ module.c
@@ -2,28 +2,24 @@
 #include <Python.h>
 #include <stdio.h>
 
-void python_module_init()
-{
-	Py_Initialize();
-}
-
 const char* python_module_call(struct python_module* p, const char* script, const char* func)
 {
+	Py_Initialize();
+
 	const char* ret=NULL;
-	python_module_init();
-	
-	FILE* f=fopen(script, "r");
-	PyRun_SimpleFile(f, script);
-	fclose(f);
-	// Get script from its name
-	p->pModule=PyImport_AddModule("__main__");
+	if(!p->pModule){
+		FILE* f=fopen(script, "r");
+		PyRun_SimpleFile(f, script);
+		fclose(f);
+		p->pModule=PyImport_ImportModule("__main__");
+	}
 
 	if(p->pModule)
 	{
 		p->pDict=PyModule_GetDict(p->pModule);
+
 		// Get function name
 		p->pFunc=PyDict_GetItemString(p->pDict, func);
-		Py_XDECREF(p->pDict);
 		if(p->pFunc && PyCallable_Check(p->pFunc))
 		{
 			p->pArgs=PyTuple_New(0);
@@ -32,11 +28,13 @@ const char* python_module_call(struct python_module* p, const char* script, cons
 			{
 				ret=PyString_AsString(p->pValue);
 				Py_DECREF(p->pValue);
+				Py_XDECREF(p->pArgs);
 			}
 			else
 			{
 				Py_DECREF(p->pFunc);
 				Py_DECREF(p->pModule);
+				Py_XDECREF(p->pArgs);
 				PyErr_Print();
 				fprintf(stderr, "Python module failed!!\n");
 			}
@@ -53,7 +51,7 @@ const char* python_module_call(struct python_module* p, const char* script, cons
 		PyErr_Print();
 		fprintf(stderr, "Failed to load %s\n", script);
 	}
+
 	Py_Finalize();
-	
 	return ret;
 }
